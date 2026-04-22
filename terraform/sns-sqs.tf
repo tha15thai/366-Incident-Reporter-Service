78d75bbf06d8f1058cb7f94da827b166f6487b55
Thawanhathai Thiamthong - 6609650111 <thawanhathai.thi@dome.tu.ac.th>
2026-04-22 23:50:46 +0700

    feat(sns): add cross-account topic policies for external subscribers

--- terraform/sns-sqs.tf
+++ terraform/sns-sqs.tf
@@ -7,12 +7,108 @@ resource "aws_sns_topic" "incident_created" {
   }
 }
 
+# SNS Topic Policy - อนุญาตให้ Account เพื่อนสามารถ Subscribe ได้
+resource "aws_sns_topic_policy" "incident_created" {
+  arn = aws_sns_topic.incident_created.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowOwnerFullAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = [
+          "SNS:Publish",
+          "SNS:Subscribe",
+          "SNS:Receive",
+          "SNS:GetTopicAttributes",
+          "SNS:SetTopicAttributes",
+          "SNS:DeleteTopic",
+          "SNS:ListSubscriptionsByTopic",
+          "SNS:AddPermission",
+          "SNS:RemovePermission"
+        ]
+        Resource = aws_sns_topic.incident_created.arn
+        Condition = {
+          StringEquals = {
+            "AWS:SourceOwner" = "317315311067"
+          }
+        }
+      },
+      {
+        Sid    = "AllowCrossAccountSubscribe"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::709706489755:root"
+        }
+        Action   = ["SNS:Subscribe", "SNS:Receive"]
+        Resource = aws_sns_topic.incident_created.arn
+      }
+    ]
+  })
+}
+
+# SNS Subscription - ชี้ไปที่ Lambda เพื่อน
+# หมายเหตุ: เพื่อนต้องรัน aws lambda add-permission ก่อน แล้วค่อย uncomment และ apply อีกครั้ง
+# resource "aws_sns_topic_subscription" "incident_created_to_friend_lambda" {
+#   topic_arn = aws_sns_topic.incident_created.arn
+#   protocol  = "lambda"
+#   endpoint  = "arn:aws:lambda:us-east-1:709706489755:function:SyncIncidentHandler"
+# }
+
 resource "aws_sns_topic" "incident_status_changed" {
   name = "${var.project_name}-incident-status-changed"
   
   tags = {
     Name = "${var.project_name}-incident-status-changed"
   }
+}
+
+# SNS Topic Policy - อนุญาตให้ Account เพื่อนคนที่ 2 สามารถ Subscribe ได้
+resource "aws_sns_topic_policy" "incident_status_changed" {
+  arn = aws_sns_topic.incident_status_changed.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowOwnerFullAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action = [
+          "SNS:Publish",
+          "SNS:Subscribe",
+          "SNS:Receive",
+          "SNS:GetTopicAttributes",
+          "SNS:SetTopicAttributes",
+          "SNS:DeleteTopic",
+          "SNS:ListSubscriptionsByTopic",
+          "SNS:AddPermission",
+          "SNS:RemovePermission"
+        ]
+        Resource = aws_sns_topic.incident_status_changed.arn
+        Condition = {
+          StringEquals = {
+            "AWS:SourceOwner" = "317315311067"
+          }
+        }
+      },
+      {
+        Sid    = "AllowCrossAccountSubscribeSQS"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::439140430379:root"
+        }
+        Action   = ["SNS:Subscribe", "SNS:Receive"]
+        Resource = aws_sns_topic.incident_status_changed.arn
+      }
+    ]
+  })
 }
 
 # SQS Queues
